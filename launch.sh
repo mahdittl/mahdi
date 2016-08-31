2883c6d5227fcc05bedab41d6b277a88bed8d2c9
teamzxbot <teamezx6952@gmail.com>
2016-08-31 16:25:21 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -11,8 +11,8 @@ update() {
 
 # Will install luarocks on THIS_DIR/.luarocks
 install_luarocks() {
-  git clone https://github.com/keplerproject/luarocks.git
-  cd luarocks
+  git clone https://github.com/SEEDTEAM/TeleSeed.git -b supergroups
+  cd teleseed
   git checkout tags/v2.2.1 # Current stable
 
   PREFIX="$THIS_DIR/.luarocks"
